37c8509d6e4c3f5335d77d6c898c4d56204ef337
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-03-09 21:48:48 +0100

    Added translations

--- inc/boot.php
+++ inc/boot.php
@@ -27,6 +27,26 @@ function boot(string $plugin_launcher_file) {
     $providers = require_once $plugin_root_dir . 'configs/providers.php';
 
     /**
+     * Loads plugin translations
+     *
+     * @return void
+     */
+
+    add_action( 'plugins_loaded', function() use ($params, $plugin_launcher_file) {
+        // Load translations from the languages directory.
+        $locale = get_locale();
+
+        // This filter is documented in /wp-includes/l10n.php.
+        $locale = apply_filters( 'plugin_locale', $locale, $params['translation_key'] ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
+        load_textdomain( $params['translation_key'], WP_LANG_DIR .  key_exists('is_mu_plugin', $params) && $params['is_mu_plugin'] ? '/mu-plugins/' : '/plugins/' . $params['translation_key'] . '-' . $locale . '.mo' );
+        if( key_exists('is_mu_plugin', $params) && $params['is_mu_plugin'] ) {
+            load_plugin_textdomain( $params['translation_key'], false, dirname( plugin_basename( $plugin_launcher_file ) ) . '/languages/' );
+            return;
+        }
+        load_muplugin_textdomain( $params['translation_key'], dirname( plugin_basename( $plugin_launcher_file ) ) . '/languages/' );
+    } );
+
+    /**
      * Tell WP what to do when plugin is loaded.
      *
      */
@@ -42,6 +62,7 @@ function boot(string $plugin_launcher_file) {
 
         $wp_rocket->load( $params, $providers );
     } );
+
 
     Deactivation::set_params($params);
     Deactivation::set_providers($providers);
