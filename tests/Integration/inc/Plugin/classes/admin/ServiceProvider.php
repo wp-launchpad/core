b3fa876f4120d3bf0ea4d2116412c20253a980e4
COQUARD Cyrille <coquardcyr@gmail.com>
2024-01-07 00:59:48 +0100

    Added base to create integration tests

---
+++ tests/Integration/inc/Plugin/classes/admin/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaunchpadCore\Tests\Integration\inc\Plugin\classes\admin;
+
+use LaunchpadCore\Container\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function define()
+    {
+        // TODO: Implement define() method.
+    }
+}
---
+++ tests/Integration/inc/Plugin/classes/common/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaunchpadCore\Tests\Integration\inc\Plugin\classes\common;
+
+use LaunchpadCore\Container\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function define()
+    {
+        // TODO: Implement define() method.
+    }
+}
