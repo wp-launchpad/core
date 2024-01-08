99b15a51b5518d489d125d8154234ae66e8532c0
COQUARD Cyrille <coquardcyr@gmail.com>
2024-01-08 19:34:34 +0100

    Added prefix test

---
+++ tests/Integration/inc/Container/PrefixAwareInterface/setPrefix.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace LaunchpadCore\Tests\Integration\inc\Container\PrefixAwareInterface;
+
+use LaunchpadCore\EventManagement\EventManager;
+use LaunchpadCore\EventManagement\Wrapper\SubscriberWrapper;
+use LaunchpadCore\Plugin;
+use LaunchpadCore\Tests\Integration\inc\Container\PrefixAwareInterface\classes\PrefixAwareClass;
+use LaunchpadCore\Tests\Integration\inc\Container\PrefixAwareInterface\classes\ServiceProvider;
+use LaunchpadCore\Tests\Integration\TestCase;
+use League\Container\Container;
+
+/**
+ * @covers \LaunchpadCore\Container\PrefixAwareInterface::set_prefix
+ */
+class Test_setPrefix extends TestCase {
+
+
+    /**
+     * @var EventManager
+     */
+    protected $event_manager;
+
+    public function testShouldDoAsExpected()
+    {
+        $this->event_manager = new EventManager();
+
+        $prefix = 'test';
+
+        $container = new Container();
+
+        $plugin = new Plugin($container, $this->event_manager, new SubscriberWrapper($prefix));
+        $plugin->load([
+            'prefix' => $prefix,
+            'version' => '3.16'
+        ], [
+                ServiceProvider::class
+        ]);
+
+        $this->assertSame($prefix, $container->get(PrefixAwareClass::class)->get_prefix());
+    }
+}
